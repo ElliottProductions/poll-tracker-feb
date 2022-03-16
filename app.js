e5ea544a6ba6dae044c792865950432849db6ffd
Elliott Cheifetz <elliottsmyname@gmail.com>
2022-03-15 19:41:26 -0800

    form is working ^_^

--- app.js
+++ app.js
@@ -5,35 +5,77 @@ const aVoteDown = document.getElementById('option-a-sub');
 const bVoteDown = document.getElementById('option-b-sub');
 const endPoll = document.getElementById('end-poll');
 
-const questionInput = document.querySelector('.question-area');
-const option1Input = document.querySelector('.question-1');
-const option2Input = document.querySelector('.question-2');
+//const questionInput = document.querySelector('.question-area');
+//const option1Input = document.querySelector('.question-1');
+//const option2Input = document.querySelector('.question-2');
+
+const questionLabel = document.getElementById('question-label');
 
 const option1Label = document.getElementById('option-1-label');
 const option1Votes = document.getElementById('option-1-votes');
 const option2Label = document.getElementById('option-2-label');
 const option2Votes = document.getElementById('option-2-votes');
+const form = document.querySelector('form');
 
 
 // let state
+let question = '';
 let aVotes = 0;
 let bVotes = 0;
+let aTitle = '';
+let bTitle = '';
+
 // set event listeners
 aVoteUp.addEventListener('click', () => {
     aVotes++;
+    displayCurrentPoll();
 });
 
 bVoteUp.addEventListener('click', () => {
     bVotes++;
+    option2Votes.textContent = bVotes;
 });
 
 aVoteDown.addEventListener('click', () => {
     aVotes--;
+    displayCurrentPoll();
 });
 
 bVoteDown.addEventListener('click', () => {
     bVotes--;
+    displayCurrentPoll();
 });
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const data = new FormData(form);
+
+    question = data.get('question-area');
+    aTitle = data.get('question-1');
+    bTitle = data.get('question-2');
+
+    displayCurrentPoll();
+
+});
+
+endPoll.addEventListener('click', () => {
+    form.rest();
+
+    const poll = makePoll();
+});
+
+function displayCurrentPoll() {
+    questionLabel.textContent = question;
+    option1Label.textContent = aTitle;
+    option2Label.textContent = bTitle;
+    option1Votes.textContent = aVotes;
+    option2Votes.textContent = bVotes;
+}
+
+
+
+
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
